e3b0e6a0021219ade4ff32e266b4b551e6b364f8
Nizar <ettaheri.nizar@gmail.com>
2025-03-24 22:46:10 +0000

    first commit

---
+++ app/src/androidTest/java/com/app/areality/ExampleInstrumentedTest.kt
@@ -0,0 +1,16 @@
+package com.app.areality
+
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+
+class ExampleInstrumentedTest {
+
+    fun useAppContext() {
+        // Context of the app under test.
+
+    }
+}
